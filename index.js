b5d303f0dc5a1cc3bf9f8ba48e99297010d42747
Deepanshu Rohilla <52520071+Deepanshu-Rohilla@users.noreply.github.com>
2020-05-17 17:09:39 +0530

    Resolved collapsing issue and added "Read More"

--- index.js
+++ index.js
@@ -12,7 +12,29 @@ xhr.open('GET', `http://newsapi.org/v2/top-headlines?sources=${source}&apikey=${
 xhr.onload = function(){
     if(this.status===200){
         let json = JSON.parse(this.responseText);
-        console.log(json)
+        let articles = json.articles;
+        let newsHtml = "";
+        console.log(articles);
+        articles.forEach(function(element, index) {
+            let news = `<div class="card">
+                <div class="card-header" id="heading${index}">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse"
+                            data-target="#collapse${index}" aria-expanded="true" aria-controls="collapse${index}">
+                            ${element['title']}
+                        </button>
+                    </h2>
+                </div>
+
+                <div id="collapse${index}" class="collapse show" aria-labelledby="heading${index}"
+                    data-parent="#newsAccordion">
+                    <div class="card-body">${element['content']}. <a href = "${element['url']}" target = "_blank">Read more here</a></div>
+                </div>
+            </div>`
+            newsHtml+=news;
+        
+        });
+        newsAccordion.innerHTML = newsHtml;
     }
     else{
         console.log('Some error occured');
@@ -21,27 +43,3 @@ xhr.onload = function(){
 xhr.send();
 
 
-let news = `<div class="card">
-                <div class="card-header" id="headingOne">
-                    <h2 class="mb-0">
-                        <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse"
-                            data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                            Collapsible Group Item #1
-                        </button>
-                    </h2>
-                </div>
-
-                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
-                    data-parent="#accordionExample">
-                    <div class="card-body">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-                        3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
-                        laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin
-                        coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes
-                        anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings
-                        occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard
-                        of them accusamus labore sustainable VHS.
-                    </div>
-                </div>
-            </div>`
-        
