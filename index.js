80055727c616b527c75a0ecdcce80ab916bbf999
Deepanshu Rohilla <52520071+Deepanshu-Rohilla@users.noreply.github.com>
2020-05-17 17:22:04 +0530

    Final Touch
    Updated navbar. Improved the headings

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 console.log("Randm");
 
 // Iitialise the variables
-let source = 'bbc-news';
 let apikey = '8a04890234a0468da6ed0a9654d9a9f5';
 // Grab the news container
 let newsAccordion = document.getElementById('newsAccordion');
 // Create an ajax GET request
 const xhr = new XMLHttpRequest();
-xhr.open('GET', `http://newsapi.org/v2/top-headlines?sources=${source}&apikey=${apikey}`, true);
+xhr.open('GET', `http://newsapi.org/v2/top-headlines?country=in&apikey=${apikey}`, true);
 
 xhr.onload = function(){
     if(this.status===200){
@@ -19,14 +18,14 @@ xhr.onload = function(){
             let news = `<div class="card">
                 <div class="card-header" id="heading${index}">
                     <h2 class="mb-0">
-                        <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse"
+                        <button class="btn btn-link collapsed btn-block text-left" type="button" data-toggle="collapse"
                             data-target="#collapse${index}" aria-expanded="true" aria-controls="collapse${index}">
-                            ${element['title']}
+                            <b>Breaking News ${index+1} : </b> ${element['title']}
                         </button>
                     </h2>
                 </div>
 
-                <div id="collapse${index}" class="collapse show" aria-labelledby="heading${index}"
+                <div id="collapse${index}" class="collapse " aria-labelledby="heading${index}"
                     data-parent="#newsAccordion">
                     <div class="card-body">${element['content']}. <a href = "${element['url']}" target = "_blank">Read more here</a></div>
                 </div>
